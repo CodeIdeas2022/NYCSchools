284123bdd6979a1f4cb2ed7ce0025bfaa3bdfbd3
Raj Lokanath <raj.lokanath@dronebase.com>
2022-10-03 08:19:29 -0700

    Handle fetch disruption

--- NYCSchools/UI/ViewController.swift
+++ NYCSchools/UI/ViewController.swift
@@ -10,15 +10,16 @@ import Combine
 
 class ViewController: UIViewController {
     @IBOutlet var labelStatus: UILabel!
-    var isDataLoaded = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadData()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        guard !isDataLoaded else { return }
+    }
+
+    func loadData() {
         labelStatus.text = "Fetching data about NYC Schools. Please wait..."
         var cancellable: AnyCancellable?
         cancellable = NetworkSession.shared.sessionStatus
@@ -38,28 +39,29 @@ class ViewController: UIViewController {
         Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { _ in
             request = Schools.shared.fetch()
                 .sink { [weak self] result in
+                    let retry: Bool
                     switch result {
                     case .finished:
-                        self?.isDataLoaded = true
+                        retry = false
                         self?.presentSchools()
                     case .failure(let e):
                         self?.labelStatus.text = "Failed to fetch and store data.\n\(e.localizedDescription)"
+                        retry = true
                     }
                     request = nil
+                    cancellable?.cancel()
+                    if retry {
+                        self?.loadData() // Try again
+                    }
                 } receiveValue: { _ in
                 }
         }
     }
-    
     func presentSchools() {
         func display() {
             guard let window = UIApplication.shared.keyWindow else { return }
             let vc = SchoolsListViewController()
             window.rootViewController = vc
-//            UIView.transition(from: view, to: vc.view, duration: 0.5, options: .transitionCurlDown) { _ in
-//                window.rootViewController = vc
-//            }
-            
         }
         DispatchQueue.executeInMain {
             display()
